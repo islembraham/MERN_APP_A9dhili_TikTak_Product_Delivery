28d06b009140542e59189a8cb61750c9ac532af7
islembraham <islem.braham@esprit.tn>
2021-11-12 13:58:45 +0100

    delete and update repo

---
+++ A9dhili-tiktak-WebApp/client/src/components/meals/OrderFooter.js
@@ -0,0 +1,34 @@
+import * as PropTypes from "prop-types";
+import React from "react";
+
+
+export function OrderFooter(props) {
+  const { disabled, onSubmit, orderTotal } = props;
+  return (
+    <div style={{ backgroundColor: "#D2E6EA" }} className="footer-copyright">
+      <div className="container">
+        <a className="waves-effect waves-light btn">
+          Order Total: {orderTotal ? `$${orderTotal}` : 0}
+        </a>
+      </div>
+      {orderTotal > 0 && (
+        <button
+          className={`btn waves-effect waves-light ${
+            disabled ? "disabled" : null
+          }`}
+          type="submit"
+          name="action"
+          onClick={onSubmit}
+        >
+          Order Now<i className="material-icons right">send</i>
+        </button>
+      )}
+    </div>
+  );
+}
+
+OrderFooter.propTypes = {
+  orderTotal: PropTypes.any,
+  disabled: PropTypes.bool,
+  onSubmit: PropTypes.func
+};
